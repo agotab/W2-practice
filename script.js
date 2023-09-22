382500457b6d7977be9434228ab481a993a875a3
agotab <agota0201@gmail.com>
2023-09-22 20:06:59 +0200

    practiced fetch and flex

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-console.log('loaded')
+/* console.log('loaded')
 
 let rootElement = document.querySelector('#root')
-/* console.log(rootElement) */
+console.log(rootElement) 
 
 let htmlContent = "<h2>Hello world, my name is Agota</h2>"
 
-/* rootElement.insertAdjacentHTML('beforeend', htmlContent) */
+rootElement.insertAdjacentHTML('beforeend', htmlContent) 
 
-/* let counter = 0;
+let counter = 0;
 
 while (counter < 10) {
     rootElement.insertAdjacentHTML('beforeend', htmlContent);
     counter++;
-} */
+} 
 
 let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes", "törköly"]
 
 for (let index = 0; index < array.length; index++) {
     rootElement.insertAdjacentHTML('beforeend', `<h2> ${array[index]} </h2>`)
-}
+} */
 
+console.log("loaded");
+
+let rootElement = document.querySelector("#root");
+
+fetch("https://restcountries.com/v3.1/all")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data); //itt tudunk a kapott adattal dolgozni
+
+    for (let index = 0; index < data.length; index++) {
+      let countryName = data[index].name.common;
+      let countryPop = data[index].population;
+      let countryArea = data[index].area;
+      rootElement.insertAdjacentHTML(
+        "beforeend",
+        `
+            <div class="country">
+                <h2> ${countryName} </h2>
+                <p> population: ${countryPop} </p>
+                <p> area: ${countryArea}km2 </p>
+            </div>
+        `
+      );
+    }
+  });
